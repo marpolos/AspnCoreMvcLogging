a53f84859209111d25f46055ea69476ca1017dcf
Marcelle Monteiro <monteiro.bio@outlook.com>
2022-11-24 18:05:18 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AspnCoreMvcLogging.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
